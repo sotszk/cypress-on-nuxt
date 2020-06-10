1fece6a27baeaa0fddbfca9c75eb1c72ac04b6b6
SotaSuzuki <3.3sota.suzuki@gmail.com>
2020-06-10 18:41:11 +0900

    update workflow to test with cypress

---
+++ cypress/integration/example.spec.js
@@ -0,0 +1,36 @@
+/// <reference types="Cypress" />
+
+describe('My First Test', () => {
+  it('visit localhost', () => {
+    cy.visit('http://localhost:3000')
+  })
+  // it('Does not do much', () => {
+  //   expect(true).to.equal(true)
+  // })
+  // it('visit cypress.io', () => {
+  //   cy.visit('https://example.cypress.io')
+  // })
+  // it('find the content "type"', () => {
+  //   cy.visit('https://example.cypress.io')
+  //   cy.contains('type')
+  // })
+  // it('clicks', () => {
+  //   cy.visit('https://example.cypress.io')
+  //   cy.contains('blur').click()
+  // })
+  // it('clicks window and assert', () => {
+  //   cy.visit('https://example.cypress.io')
+  //   cy.contains('window').click()
+  //   cy.url().should('include', '/commands/window')
+  // })
+  // it('gets, types, and asserts', () => {
+  //   cy.visit('https://example.cypress.io')
+  //   cy.root().contains('type').click()
+
+  //   cy.url().should('include', '/commands/actions')
+
+  //   cy.get('.action-email')
+  //     .type('fake@email.com')
+  //     .should('have.value', 'fake@email.com')
+  // })
+})
